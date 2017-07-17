0a3354dc8d98f80857ada3d443e57ddf61620c90
Renato <renatocrobledo@gmail.com>
2017-07-17 18:22:50 -0500

    wip

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+describe('Api', ()=> {
+
+  beforeEach(()=>{
+    window.fetch = jest.fn().mockImplementation((e) => {
+      const response = {
+        json: () => []
+      }
+      return Promise.resolve(response);
+    });
+  });
+
+  it('renders without crashing', () => {
+     const div = document.createElement('div');
+     ReactDOM.render(<App />, div);
+  });
 });
